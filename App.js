fe51455326996d540de0f5d220313d8ed787d49c
Graeme <graemster@gmail.com>
2020-01-02 18:14:41 -0600

    add tab component

--- App.js
+++ App.js
@@ -3,20 +3,21 @@ import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { Button } from "react-native-elements";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import Bubble from "./src/components/Bubble";
+import Tab from "./src/components/Tab";
 
 export default function App() {
   const [state, setState] = useState({
     attackerIs: null,
-    defenderSelected: false,
+    defender: null,
     canadaArmies: 5,
-    mericaArmies: 12
+    mericaArmies: 12,
+    showTab: false
   });
 
   const compareRolls = () => {
     const rollDie = () => {
       return Math.floor(Math.random() * 6 + 1);
     };
-
     const a = rollDie();
     const d = rollDie();
     console.log("attacker rolled: ", a);
@@ -26,28 +27,37 @@ export default function App() {
     return false;
   };
 
-  const handleClick = defender => {
+  const doAttack = (defender) => {
+    const attackSuccessful = compareRolls();
+
+    if (attackSuccessful) {
+      console.log("attack success!");
+      setState(prevState => ({
+        ...prevState,
+        [defender]: prevState[defender] - 1,
+        attackerIs: null,
+        defender: null,
+        showTab: false
+      }));
+    } else {
+      console.log("attack failed!");
+      setState(prevState => ({
+        ...prevState,
+        [state.attackerIs]: prevState[state.attackerIs] - 1,
+        attackerIs: null,
+        defender: null,
+        showTab: false
+      }));
+    }
+  };
+
+  const countryClick = defender => {
     if (state.attackerIs) {
       if (state.attackerIs === defender) {
-        setState({ ...state, attackerIs: null });
+        setState({ ...state, attackerIs: null, defender: null, showTab: false });
       } else {
-        const attackSuccessful = compareRolls();
-
-        if (attackSuccessful) {
-          console.log("attack success!");
-          setState(prevState => ({
-            ...prevState,
-            [defender]: prevState[defender] - 1,
-            attackerIs: false
-          }));
-        } else {
-          console.log("attack failed!");
-          setState(prevState => ({
-            ...prevState,
-            [state.attackerIs]: prevState[state.attackerIs] - 1,
-            attackerIs: false
-          }));
-        }
+        // After selecting a country to attack with AND a country to be attacked
+        setState({ ...state, showTab: true, defender });
       }
     } else {
       setState({ ...state, attackerIs: defender });
@@ -56,6 +66,9 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      {state.showTab ? (
+        <Tab doAttack={doAttack} state={state} setState={setState} defender={state.defender} />
+      ) : null}
       <Text>
         {state.attackerIs ? "Choose defender broh!" : "Choose attacker broh!"}
       </Text>
@@ -64,14 +77,14 @@ export default function App() {
         icon={<Bubble value={state.canadaArmies} />}
         title="Canada"
         raised
-        onPress={() => handleClick("canadaArmies")}
+        onPress={() => countryClick("canadaArmies")}
       />
       <Button
         containerStyle={styles.country}
         icon={<Bubble value={state.mericaArmies} />}
         title="'Merica"
         raised
-        onPress={() => handleClick("mericaArmies")}
+        onPress={() => countryClick("mericaArmies")}
       />
     </View>
   );
---
+++ src/components/Tab.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { Button } from "react-native-elements";
+
+const Tab = ({ defender, state, setState, doAttack }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        How many armies do you wish to attack {defender} with?
+      </Text>
+      <View style={styles.buttonContainer}>
+        <Button
+          containerStyle={styles.button}
+          onPress={() => doAttack(defender)}
+          title="2"
+        />
+        <Button
+          containerStyle={styles.button}
+          onPress={() => doAttack(defender)}
+          title="3"
+        />
+        <Button
+          containerStyle={styles.button}
+          onPress={() => doAttack(defender)}
+          title="1"
+        />
+        <Button
+          containerStyle={styles.button}
+          onPress={() => doAttack(defender)}
+          title="ALL"
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 50,
+    width: Dimensions.get("window").width,
+    height: 100,
+    backgroundColor: "#000000",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  buttonContainer: {
+    width: Dimensions.get("window").width - 50,
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  button: {
+    width: 75
+  },
+  text: {
+    color: "#fff",
+    fontSize: 18
+  }
+});
+
+export default Tab;
